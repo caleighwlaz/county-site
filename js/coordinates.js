360b686d08966959dfe47412fba34988d807476d
Caleigh <cwlazlow@villanova.edu>
2026-09-23 23:54:59 -0400

    HW3

--- js/coordinates.js
+++ js/coordinates.js
@@ -5,6 +5,7 @@ const myCounty = {
     xmax: -73.965414,
     ymin: 39.475198,
     ymax: 40.172402,
+    ymid: 39.8238,
 }
 console.log(myCounty)
 
@@ -47,3 +48,46 @@ function checkCoords() {
  }
  document.getElementById("result").textContent = result;
 }
+
+const checkBtn = document.querySelector("#check-btn");
+const resultDiv = document.querySelector("#result");
+checkBtn.addEventListener("click", () => {
+   let latInput = document.getElementById('lat-input').value;
+   let lonInput = document.getElementById('lon-input').value;
+   
+   latInput = parseFloat(latInput);
+   lonInput = parseFloat(lonInput);
+
+   let result;
+    if(isNaN(latInput) || isNaN(lonInput)) {
+     result = "Please enter numbers for latitude and longitude.";
+     document.getElementById("result").textContent = result;
+     return result;
+   }
+
+   if(lonInput >= myCounty.xmin && lonInput <= myCounty.xmax) {
+    if(latInput >= myCounty.ymin && latInput <= myCounty.ymax) {
+        result = "YES! Those coordinates are INSIDE Ocean County!";
+        resultDiv.classList.add("result-inside");
+        if(latInput >= myCounty.ymid) {
+         result += " You're in the northern half of Ocean County";
+        }
+        else {
+         result += " You're in the southern half of Ocean County";
+        }
+    }
+   }
+   else if(latInput <= -90 || latInput >= 90) {
+    result = "Please enter valid numbers for latitude and longitude.";
+    resultDiv.classList.add("result-wrong");
+   }
+   else if(lonInput <= -180 || lonInput >= 180) {
+    result = "Please enter valid numbers for latitude and longitude.";
+    resultDiv.classList.add("result-wrong");
+   }
+   else {
+    result = "NO. Those coordinates are OUTSIDE Ocean County.";
+    resultDiv.classList.add("result-outside");
+   }
+   document.getElementById("result").textContent = result;
+})
